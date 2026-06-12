ec43d1a5af0692200b54808a1d02b7c545f51eaa
Ely Torres Neto <93855901+netoe1@users.noreply.github.com>
2026-06-12 20:51:59 -0300

    Create portugol.h

---
+++ portugol.h
@@ -0,0 +1,23 @@
+#ifndef PORTUGOL_H
+#define PORTUGOL_H
+
+#define inteiro int
+#define caractere char
+#define real float
+#define escreva printf
+#define escanear scanf
+#define retornar return
+#define funcao_principal main 
+#define logico bool
+#define depende if 
+#define senao else 
+#define incluir include
+#define enquanto while
+#define troca switch
+#define para for
+#define caso case
+#define quebra break
+#define saida_padrao default
+#define indefinido void 
+
+#endif
